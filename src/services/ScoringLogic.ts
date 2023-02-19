7e22a8993ef04b76e81f584df7865ea5b57df512
ppedneka <pratik_pednekar@mcafee.com>
2023-02-19 15:56:50 +0530

    fix - lower case input

--- src/services/ScoringLogic.ts
+++ src/services/ScoringLogic.ts
@@ -1,7 +1,7 @@
 export class ScoringLogic {
 
     calculateBasicScore(word: string) {
-        const score = word.split('').reduce((total, char) => {
+        const score = word.toLowerCase().split('').reduce((total, char) => {
             const val = (char.charCodeAt(0) == 32) ? 0 : (char.charCodeAt(0) - 96)
             return total + val
         }, 0)
